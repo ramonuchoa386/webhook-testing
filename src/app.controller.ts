92cd2d1d668fb28efae5e3120d9b50e7b7e6677c
Ramon Uchôa Gonçalves de Pinho <ramonuchoa386@gmail.com>
2026-04-02 21:04:32 +0000

    feat: adicionando validação do header para determinar response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,20 +3,29 @@ import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  @Get('webhook')
-  handleGetWebhook(
+  @Post('webhook')
+  handleWebhook(
     @Query('validationToken') queryToken: string,
     @Headers('client-request-id') headerToken: string,
     @Res() res: Response,
+    @Body() body: any
   ) {
     const token = queryToken || headerToken;
-    res.set('Content-Type', 'text/plain');
-    res.status(200).send(token);
-  }
 
-  @Post('webhook')
-  handlePostWebhook(@Body() body: any, @Res() res: Response) {
+    if(token) {
+        res.set('Content-Type', 'text/plain');
+        res.status(200).send(token);
+
+        return;
+    }
+
     console.log('Received webhook payload:', JSON.stringify(body, null, 2));
     res.status(202).send();
   }
+
+//   @Post('webhook')
+//   handlePostWebhook(@Body() body: any, @Res() res: Response) {
+//     console.log('Received webhook payload:', JSON.stringify(body, null, 2));
+//     res.status(202).send();
+//   }
 }
